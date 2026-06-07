d9770a80248e4a27b744609d2580f2b2f45490d4
Melodi <83541357+melodiella@users.noreply.github.com>
2026-06-07 04:01:27 -0400

    Add files via upload

---
+++ practicetools/gearCreator.py
@@ -0,0 +1,47 @@
+from maya import cmds
+
+def createGear(teeth=10, length=0.3):
+    '''
+    This function creates a gear with the given parameters
+    Args:
+        teeth:
+        length:
+
+    Returns:
+        A tuple of the transform, constructor and extrude node
+    '''
+    print ("Creating Gear", teeth, length)
+    spans = teeth * 2
+
+    transform, constructor = cmds.polyPipe(subdivisionsAxis=spans)
+
+    sideFaces = range(spans*2, spans*3, 2)
+
+    cmds.select(clear=True)
+
+    for face in sideFaces:
+        cmds.select('%s.f[%s]' % (transform, face), add=True)
+
+    extrude = cmds.polyExtrudeFacet(localTranslateZ=length)[0]
+    print (extrude)
+    return transform, constructor, extrude
+
+def changeTeeth(constructor, extrude, teeth=10, length=0.3):
+    spans = teeth * 2
+
+    cmds.polyPipe(constructor, edit=True,
+                  subdivisionsAxis=spans)
+
+    sideFaces = range(spans*2, spans*3, 2)
+    faceNames = []
+
+    for face in sideFaces:
+        faceName = 'f[%s]' % (face)
+        faceNames.append(faceName)
+
+    cmds.setAttr('%s.inputComponents' % (extrude),
+                  len(faceNames),
+                  *faceNames,
+                 type="componentList")
+
+    cmds.polyExtrudeFacet(extrude, edit=True, ltz=length)
