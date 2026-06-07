5f219d94b3650e38d64a0931b6382550757ff0bc
Melodi <83541357+melodiella@users.noreply.github.com>
2026-06-07 05:11:19 -0400

    Add files via upload

---
+++ practicetools/simplecuberig.py
@@ -0,0 +1,22 @@
+from maya import cmds
+
+cube = cmds.polyCube()
+
+cubeShape = cube[0]
+
+print (cubeShape)
+
+circle = cmds.circle()
+
+circleShape = circle[0]
+
+cmds.parent(cubeShape, circleShape)
+
+cmds.setAttr(cubeShape+".translate", lock=True)
+
+cmds.setAttr(cubeShape+".rotate", lock=True)
+
+cmds.setAttr(cubeShape+".scale", lock=True)
+
+
+cmds.select(circleShape)
