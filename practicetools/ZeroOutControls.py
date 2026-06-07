46a5460e37a55e4522e33608d0e7c7a8daf4fa66
Melodi <83541357+melodiella@users.noreply.github.com>
2026-06-07 04:03:55 -0400

    Add files via upload

---
+++ practicetools/ZeroOutControls.py
@@ -0,0 +1,38 @@
+```
+Zero Out Controls Script by Melodi
+```
+
+# Explanation:
+import maya.cmds as cmds
+
+# gets selection on controls
+control_selection = cmds.ls(long=True, type="nurbsCurve")
+
+cmds.select(control_selection, replace=True)
+
+# loop through those controls
+for shape in control_selection:
+    offset_grp = f"{ctrl}_offset_grp"
+
+# store parent information
+    transform = cmds.listRelatives(shape, parent=True)[0]
+
+    cmds.group(empty=True, name=offset_grp)
+
+# create a freeze/offset group
+
+    cmds.matchTransform(offset_grp, ctrl, pos=True, rot=True, scale=False)
+
+# Parent offset group back under original hierarchy
+    cmds.parent(offset_grp, transform)
+# selects attributes on control
+
+# changes attributes to 0 for translate and rotate, scale stays 1, 1, 1
+
+    cmds.setAttr(ctrl + ".translateX", 0)
+    cmds.setAttr(ctrl + ".translateY", 0)
+    cmds.setAttr(ctrl + ".translateZ", 0)
+
+    cmds.setAttr(ctrl + ".rotateX", 0)
+    cmds.setAttr(ctrl + ".rotateY", 0)
+    cmds.setAttr(ctrl + ".rotateZ", 0)
